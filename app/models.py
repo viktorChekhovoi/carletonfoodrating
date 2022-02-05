18d6808fc20b40fa2f0c929beacf823c9dcf64b2
victorChekhovoy <viktor.chekhovoi@gmail.com>
2022-02-05 20:58:42 +0200

    Added a migration database

--- app/models.py
+++ app/models.py
@@ -3,18 +3,18 @@ from app import db
 class DiningHall(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=False)
-    station1 = db.column(db.String(128), unique=True)
-    station2 = db.column(db.String(128), unique=True)
-    station3 = db.column(db.String(128), unique=True)
-    station4 = db.column(db.String(128), unique=True)
-    station5 = db.column(db.String(128), unique=True)
-    station6 = db.column(db.String(128), unique=True)
+    station1 = db.Column(db.String(128), unique=True)
+    station2 = db.Column(db.String(128), unique=True)
+    station3 = db.Column(db.String(128), unique=True)
+    station4 = db.Column(db.String(128), unique=True)
+    station5 = db.Column(db.String(128), unique=True)
+    station6 = db.Column(db.String(128), unique=True)
 
     def __repr__(self):
         return f'<{self.name} Dining Hall>'
 
 
-class Station(db.model):
+class Station(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=False)
     food = db.Column(db.Text)
@@ -23,7 +23,7 @@ class Station(db.model):
         return f'<{self.name} Station>'
 
 
-class Food(db.model):
+class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=False)
     rating = db.Column(db.Float)
