9b5a9ee88d057e4b72cd748c256a36b1480878d1
victorChekhovoy <viktor.chekhovoi@gmail.com>
2022-02-05 21:30:03 +0200

    Working db

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class DiningHall(db.Model):
     #station5 = db.Column(db.String(128), unique=True)
     #station6 = db.Column(db.String(128), unique=True)
 
-    stations = db.relationship('Station', backref='Location', lazy='dynamic')
+    stations = db.relationship('Station', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'<{self.name} Dining Hall>'
@@ -20,8 +20,9 @@ class Station(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=False)
     #food = db.Column(db.Text)
+    locationId = db.Column(db.Integer, db.ForeignKey('dining_hall.id'))
 
-    foods = db.relationship('Food', backref='Station', lazy='dynamic')
+    foods = db.relationship('Food', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'<{self.name} Station>'
@@ -31,6 +32,7 @@ class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=False)
     rating = db.Column(db.Float)
+    stationId = db.Column(db.Integer, db.ForeignKey('station.id'))
 
     def __repr__(self):
         return f'{self.name} is rated {self.rating} our of 10'
