a7618988d03c88604c26122f3a09a89fd5ec09e7
victorChekhovoy <viktor.chekhovoi@gmail.com>
2022-02-06 04:45:28 +0200

    Pushing all of my changes

--- app/routes.py
+++ app/routes.py
@@ -4,44 +4,62 @@ from app import app, db
 from app.forms import RateForm
 from app.models import DiningHall, Station, Food, Rating
 from flask import render_template, flash, redirect
+from app.getMenu import Parser
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    food = {"Pizza": [{"name": "Cheese", "rate": 9}, {"name": "Pepperoni", "rate": 8}]}
+
+def getBurtonJson():
+    parser = Parser("burton")
+    burtonMenu = {}
+    burton = {"Burton": {"status": parser.isOpen(), "menu": burtonMenu}}
+
+
+def makeForms(menus: str):
     forms = dict()
-    for station in food:
+    for station in menus:
         #print(station)
-        for foodItem in food[station]:
+        for foodItem in menus[station]:
             if station not in forms:
                 forms[station] = {}
             newForm = RateForm()
             newForm.name = f'{station}:{foodItem}'
             forms[station][foodItem["name"]] = newForm
+    return forms
 
-    print(forms)
 
+def updateAverage(food):
+    food_db = Food.query.filter_by(name=food).all()
+    if len(food_db) != 0:
+        food_db = food_db[0]
+        newRating = Rating(rating=food.rating.data, foodRated=food_db)
+        db.session.add(newRating)
+        total = 0
+        numRatings = 0
+        allRatings = Rating.query.filter_by(foodRated=food_db).all()
+        for rating in allRatings:
+            numRatings += 1
+            total += rating.rating
+
+        averageRating = total/numRatings
+        return averageRating
+    return food.averageRating
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    
+    food = {"Pizza": [{"name": "Cheese", "rate": 9}, {"name": "Pepperoni", "rate": 8}]}
+    
+    forms = makeForms(food)
+    parser = Parser("burton")
+    print(parser.isOpen())
+    parser.getMenuHtml()
     for station in forms:
         for food in forms[station]:
             if forms[station][food].validate_on_submit():
-                #station_db = Station.query.filter_by(name=station).all()[0]
-                food_db = Food.query.filter_by(name=food).all()
-                if len(food_db) != 0:
-                    food_db = food_db[0]
-                    newRating = Rating(rating=food.rating.data, foodRated=food_db)
-                    db.session.add(newRating)
-                    total = 0
-                    numRatings = 0
-                    allRatings = Rating.query.filter_by(foodRated=food_db).all()
-                    for rating in allRatings:
-                        numRatings += 1
-                        total += rating.rating
-
-                    averageRating = total/numRatings
-
-                    food.averate_rating = averageRating
-
+                food.averate_rating = updateAverage(food)
                 return redirect('/index')
                 
     food = {"Pizza": [{"name": "Cheese", "rate": 9}, {"name": "Pepperoni", "rate": 8}]}
